3ac5bd25731bab179a43d348704093ff0641d0dc
Ilya_Baklanov <60309676+Ilya-Baklanov@users.noreply.github.com>
2021-09-24 10:54:09 +0300

    feat: add newProject

---
+++ packages/newProject/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+import Footer from './Footer';
+
+export default () => (
+  <Container fixed>
+    <Footer />
+  </Container>
+);
